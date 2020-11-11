6db53c77b3a937ab0ec7c59abeeae62fd541ce3e
Muskan@9347 <mbindal250902@gmail.com>
2020-11-11 14:20:56 +0700

    Edited main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """
 Run the math adventure.
 
-#long descreiption here
+You are playing a student who is struggling with an upcoming exam, so you look
+upon the internet for some practice problems. You find a mysterious maths
+website called “Nanastoy’s math world”. You click on the website and see a
+strange bright light coming out of your screen. The bright light teleports you
+into the strange dimension - Nanastoy's Math World. To go back to your
+normal world, you need to search for the “exit portal”, located randomly in the
+4 corners of the math world. However; while you are searching for the portal,
+you will need to encounter the “math monsters” and “The professor Nanastoy”.
+There will also be loot boxes that contain both good and bad items,
+so you will have to pray for good fortunes. Can you take on the challenges of
+the math world and make it out alive, or will you die trying, stuck in the
+alternate reality forever...
 
 Authors : Thanatis Dullayateeraroj < MackyTD : thanatisd2545@gmail.com >
           Muskan Bindal < Muskan9347 : mbindal250902@gmail.com >
