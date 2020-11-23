6b6497902dc94948b369bb33b16cd33373827038
Jidapa Kawa Cheng <tuckkiesss@Jidapas-MacBook-Air.local>
2020-11-23 18:13:52 +0700

    ENDING COMPLETED

--- nanastoy_mathworld/end.py
+++ nanastoy_mathworld/end.py
@@ -3,15 +3,45 @@ import pyfiglet
 
 
 def ending1():  # didn't click the link
-    say("some story here")
+    say("You thought to yourself that the website is too suspiscious")
+    say("You decided to move on to the next website on the internet findings")
+    say("You ended up on the math memes website")
+    say("And you spent the whole night looking at memes...")
+    say("WHAT A SHAME", "red")
+    say("Not only you didn't practice for the exam")
+    say("But you're also woke up late for the exam")
+    say("And you failed the e")
+    say("consequences......", "red")
 
 
 def ending2():  # lost all HP
-    say("some story here")
+    say("You feel exhausted after you lost the fight")
+    say("You don't know what just happened")
+    say("But the number of your 'hp' reached 0")
+    say("That might be the reason why you feel so sleepy")
+    say("Everything is slowly fading before your eyes")
+    say("You don't think you will ever wake up again if you fell alseep")
+    say("YOU ARE RIGHT", "red")
+    say("YOU DIED", "red")
+    say("DON'T GIVE UP, STAY DETERMINATED!")
 
 
 def ending3():  # win
-    say("some story here")
+    say("After a long journey")
+    say("You are finally stand infront of the exit portal")
+    say("The light of the portal is so bright")
+    say("Just like your future after tomorrow exam")
+    say("You recalled everything you went through")
+    say("All the quiz you did")
+    say("All the monsters you fought")
+    say("And lastly, professor Nanastoy...")
+    say("It fills you with DETERMINATION for your exam")
+    say("You slowly walked into the portal, hoping to get back")
+    say("The portal teleported you back to your room")
+    say("You thought it was just a weird dream")
+    say("Yet, you feels like you are prepared for tomorrow exam")
+    say("You aced the calculus exam on the next day")
+    say("You live happiliy ever after")
 
 
 def ending4():  # got the instant lost item
